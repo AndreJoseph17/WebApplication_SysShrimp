e7b42e807c83662a61f1c2c7501dbfd530f1fd70
HP <HP@LAPTOP-ES9LVVKO>
2022-07-19 23:47:34 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication_SysShrimp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication_SysShrimp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
